5aa3e9817f77a89db2620189cce6d40e5ed9519f
dmurawski <dmurawski018@gmail.com>
2024-10-30 19:18:16 +0100

    Start blog api project, add custom user model, post app, add post model

---
+++ accounts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
