a0d3fba563e9865db824ac6b0652002a3ebe8c7c
dmurawski <dmurawski018@gmail.com>
2024-10-31 13:21:06 +0100

    add user endpoints

--- posts/urls.py
+++ posts/urls.py
@@ -2,14 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "<int:pk>/",
-        views.PostDetail.as_view(),
-        name="post_detail",
-    ),
-    path(
-        "",
-        views.PostList.as_view(),
-        name="post_list",
-    ),
+    path("<int:pk>/", views.PostDetail.as_view(), name="post_detail"),
+    path("", views.PostList.as_view(), name="post_list"),
+    # user
+    path("users/", views.UserList.as_view(), name="user_list"),
+    path("users/<int:pk>/", views.UserDetial.as_view(), name="user_detail"),
 ]
